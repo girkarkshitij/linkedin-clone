74499b556d5c4eef997eca43215caca2c6adfc46
Kshitij <girkarkshitij@gmail.com>
2022-10-19 19:24:33 +0530

    add login component

---
+++ src/Login.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import {useState} from 'react';
+import {auth} from './firebase';
+import './Login.css';
+import Logo from './assets/linkedin.png';
+import {login} from './features/userSlice';
+import {useDispatch} from 'react-redux';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [profilePic, setProfilePic] = useState('');
+  const [name, setName] = useState('');
+  const dispatch = useDispatch();
+
+  const register = () => {
+    if (!name) {
+      return alert('Please enter your full name');
+    }
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userAuth) => {
+        userAuth.user
+          .updateProfile({
+            displayName: name,
+            photoURL: profilePic,
+          })
+          .then(() => {
+            dispatch(
+              login({
+                email: userAuth.user.email,
+                uid: userAuth.user.uid,
+                displayName: name,
+                photoUrl: profilePic,
+              })
+            );
+          });
+      })
+      .catch((error) => alert(error));
+  };
+
+  const loginToApp = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className='login'>
+      <img src={Logo} alt='' />
+      <form>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder='Full name is required'
+          type='text'
+        />
+        <input
+          value={profilePic}
+          onChange={(e) => setProfilePic(e.target.value)}
+          placeholder='Profile pic URL (optional)'
+          type=''
+        />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder='Email'
+          type='email'
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder='Password'
+          type='password'
+        />
+        <button type='submit' onClick={loginToApp}>
+          Sign In
+        </button>
+      </form>
+
+      <p>
+        Not a member?{' '}
+        <span className='login__register' onClick={register}>
+          Register Now
+        </span>
+      </p>
+    </div>
+  );
+}
+
+export default Login;
